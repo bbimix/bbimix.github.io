93b9e4ecf1d8dbdaba38c67113df4d53e17b756e
bbimix <2505520629@qq.com>
2020-10-24 23:50:19 +0800

    bbimix 2.0

---
+++ JS/index.js
@@ -0,0 +1,63 @@
+
+    $(document).ready(function () {
+    var count=0;
+//     $("#images img").mouseover(function () {
+//     $(this).stop().fadeTo("slow",0);
+// });
+//     $("#images img").mouseout(function () {
+//     $(this).stop().fadeTo("slow",1)
+});
+    $("#switch").click(function(){
+    count++;
+    // let color = $("body").css("backgroundColor");
+    if(count%2==0){
+    $("#switch1").stop().fadeOut(1500);
+    $("#switch2").stop().fadeOut(500);
+}else{
+    $("#switch1").stop().fadeIn(500);
+    $("#switch2").stop().fadeIn(1500);
+}
+});
+
+    //style dark and light.
+    $("#switch1").click(function () {
+    $("body").css("backgroundColor","black");
+});
+    $("#switch2").click(function () {
+    $("body").css("backgroundColor","white");
+});
+
+    //THE bbimix
+    $(window).scroll(function(){
+    // $(".ftr").css({opacity:0.005*(-1800+$(window).scrollTop())})
+    var footerHeight = $("#footer1").height();
+    if($("#footer1").offset().top-$(window).scrollTop()<$(window).height()-$("#footer1").height()+150){
+    $(".ftr").css({opacity:1})
+}else{
+    $(".ftr").css({opacity:0})
+}
+
+})
+
+});
+
+    //加载完成后再执行 否则加载完成前会找不到DOM元素
+    $(document).ready(function(){
+    const adidas = document.querySelector(".adidas");
+    const navmenu = document.querySelector(".navmenu");
+    const menuli = document.querySelectorAll(".navmenu li");
+    adidas.addEventListener("click",()=>{
+    //点击三条滑出菜单栏。
+    adidas.classList.toggle("active");
+    navmenu.classList.toggle("open");
+
+    menuli.forEach((item,index) => {
+    if(item.style.animation){
+    item.style.animation = "";
+}else{
+    item.style.animation = `0.3s ease-in slipIn forwards ${index * 0.2 + 0.5}s`;
+}
+});
+})
+
+});
